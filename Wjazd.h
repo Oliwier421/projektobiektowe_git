85714edd5bbd3683d080d72be1cac5a6ee6ea2bd
julias-2642 <julia.spala@op.pl>
2026-05-23 17:12:53 +0200

    Update Wjazd.h

--- Wjazd.h
+++ Wjazd.h
@@ -1,11 +1,18 @@
 #pragma once
+#include <string>
 
 class Wjazd {
-    private:
-        int czas_poczatku, czas_konca;
-    public:
-        Wjazd(int czas_p, int czas_wjazdu);
-        void wydluzWjazd(int czas);
-        int getCzasPoczatku() const;
-        int getCzasKonca() const;
+private:
+    int czas_poczatku;
+    int czas_konca;
+    std::string klientLogin;
+
+public:
+    Wjazd(int czas_p, int czas_wjazdu, const std::string& login);
+    void wydluzWjazd(int czas);
+
+    int getCzasPoczatku() const;
+    int getCzasKonca() const;
+    std::string getKlientLogin() const;
+    int getPozostalyCzasWjazdu(int czas_obecny) const;
 };
