a34e42823313f72115a107d178f646672ddf7995
julias-2642 <julia.spala@op.pl>
2026-05-23 17:15:10 +0200

    Update Wjazd.cpp

--- Wjazd.cpp
+++ Wjazd.cpp
@@ -1,8 +1,15 @@
 #include "Wjazd.h"
 
-Wjazd::Wjazd(int czas_p, int czas_wjazdu) :
-    czas_poczatku(czas_p), czas_konca(czas_p+czas_wjazdu) {}
+Wjazd::Wjazd(int czas_p, int czas_wjazdu, const std::string& login)
+    : czas_poczatku(czas_p), czas_konca(czas_p + czas_wjazdu), klientLogin(login) {}
 
-void Wjazd::wydluzWjazd(int czas) {if (czas>0) czas_konca+=czas;}
+void Wjazd::wydluzWjazd(int czas) {if (czas > 0) czas_konca += czas;}
+
 int Wjazd::getCzasPoczatku() const {return czas_poczatku;}
 int Wjazd::getCzasKonca() const {return czas_konca;}
+std::string Wjazd::getKlientLogin() const {return klientLogin;}
+int Wjazd::getPozostalyCzasWjazdu(int czas_obecny) const {
+    int pozostalo = czas_konca - czas_obecny;
+    if (pozostalo < 0) {return 0;}
+    return pozostalo;
+}
