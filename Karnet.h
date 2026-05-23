247e2a8d355e4d14d9661bfdfa3e34f27068f2b6
Oliwier421 <owelnerhardt@student.agh.edu.pl>
2026-05-23 18:52:17 +0200

    Update Karnet.h

--- Karnet.h
+++ Karnet.h
@@ -1,17 +1,31 @@
 #pragma once
-//#include "TypKarnetu.h"
+
 #include "Cennik.h"
 
 class Karnet {
-    private:
-        bool ulga;
-        TypKarnetu typ;
-        double cena;
-        int data_poczatku, data_konca;
-        int czas_poczatku, czas_konca;
-    public:
-        Karnet(TypKarnetu T, bool U, int czas_obecny, int data_obecna, const Cennik& cennik);
-        bool czyAktywny(int czas_obecny, int data_obecna) const;
-        int getDataKonca() const;
-        int getCzasKonca() const;
+private:
+    bool ulga;
+    bool aktywowany = false;
+
+    TypKarnetu typ;
+    double cena;
+
+    int data_poczatku = 0;
+    int data_konca = 0;
+    int czas_poczatku = 0;
+    int czas_konca = 0;
+
+public:
+    Karnet(TypKarnetu T, bool U, int czas_obecny, int data_obecna, const Cennik& cennik);
+
+    void aktywuj(int czas_obecny, int data_obecna);
+
+    bool czyAktywowany() const;
+    bool czyMoznaAktywowac(int czas_obecny) const;
+    bool czyAktywny(int czas_obecny, int data_obecna) const;
+    bool czyWygasl(int czas_obecny, int data_obecna) const;
+
+    int getDataKonca() const;
+    int getCzasKonca() const;
+    int getPozostaleSekundy(int czas_obecny, int data_obecna) const;
 };
